7123ebfa1d290cb4a132ebd50c0ed094719c6b1b
alfandi21 <alfandif123@gmail.com>
2023-05-11 05:54:56 +0700

    update Regis & database

--- Script/Src/Registrasi.php
+++ Script/Src/Registrasi.php
@@ -1,3 +1,21 @@
+<?php
+    include '../PHP/config.php'; 
+    if (isset($_POST["submit"])){
+        $Nama = $_POST["Nama"];
+        $Gender = $_POST["Gender"];
+        $NIDN = $_POST["NIDN"];
+        $Jabatan = $_POST["Jabatan"];
+        $tgl_lahir = $_POST["tgl_lahir"];
+        $Email = $_POST["Email"];
+        $NO_HP = $_POST["NO_HP"];
+        $alamat_kantor = $_POST["alamat_kantor"];
+        $no_telp_kantor = $_POST["no_telp_kantor"];
+
+        $query = "INSERT INTO tb_registrasi
+        VALUES ('','$Nama','$Gender','$NIDN','$Jabatan','$tgl_lahir','$Email','$NO_HP','$alamat_kantor','$no_telp_kantor')";
+        mysqli_query($conf, $query);
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -19,55 +37,56 @@
       </div>
       <!-- biodata -->
       <div class="konten">
-        <form action="">
+        <form action="" method="post">
           <!-- nama -->
           <input type="text" name="Nama" placeholder="Nama" required />
           <!-- gender -->
           <div class="gender">
             <p>Gender</p>
-          <input type="radio" name="Gender" placeholder="Gender" required />
+          <input type="radio" name="Gender" value="Laki-Laki" placeholder="Gender" required />
           <label for="lk">Laki-laki</label> 
-          <input type="radio" name="Gender" placeholder="Gender" required />
+          <input type="radio" name="Gender" value="Perempuan" placeholder="Gender" required />
           <label for="pr" class="">Perempuan</label>
-             </div>
+          </div>
 
           <!-- NIDN -->
           <input type="text" name="NIDN" placeholder="NIDN" />
           <!-- jabatan -->
           <input
             type="text"
-            name="Jabatan Fungsional"
+            name="Jabatan"
             placeholder="Jabatan Fungsional"
             required
           />
           <!-- tempat tanggal lahir -->
           <input
             type="date"
-            name="Tempat Tangggal Lahir"
-            placeholder="Tempat Tangggal Lahir"
+            name="tgl_lahir"
+            placeholder="Tanggal Lahir"
             required
           />
           <!-- email -->
           <input type="email" name="Email" placeholder="Email" required />
           <!-- no hp -->
-          <input type="tel" pattern="[0-9]{4}-[0-9]{4}-[0-9]{4}" name="No Hp " placeholder="No Hp (0812-3456-7890)" required />
-         <!-- alamat kantor -->
+          <input type="text" name="NO_HP" placeholder="No Hp (0812-3456-7890)" required />
+          <!-- alamat kantor -->
           <input
             type="text"
-            name="Alamat Kantor"
+            name="alamat_kantor"
             placeholder="Alamat Kantor"
             required
           />
           <!-- no telp kantor -->
           <input
             type="text"
-            name="No Telepon Kantor"
+            name="no_telp_kantor"
             placeholder="No Telepon Kantor"
             required
           />
           <!-- button save -->
           <div class="btnn">
-            <a href="Loginpage.html"><input type="submit" value="Save" /></a>
+            <input type="submit" name="submit" value="Save">
+            <!--<a href="Loginpage.html"><input name="submit" type="submit" value="Save" /></a>-->
           </div>
         </form>
       </div>
